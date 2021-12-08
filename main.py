c8cfc8722d9ea4195c46b024dee5c0dd44017998
CatMe0w <CatMe0w@live.cn>
2021-12-09 02:26:12 +0800

    Refactor

--- main.py
+++ main.py
@@ -98,7 +98,6 @@ for i in range(1, MAX_PAGE + 1):
     for j in range(1, 31):
         url_params = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@href'.format(j))[0][3:]
-        thread_id = re.findall('.+?(?=\?)', url_params)[0]
         title = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@title'.format(j))[0]
         content_preview = tree.xpath(
@@ -107,12 +106,8 @@ for i in range(1, MAX_PAGE + 1):
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[1]/a/text()'.format(j))[0][4:]
         nickname_raw = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[2]/a/text()'.format(j))[0][3:]
-        nickname = get_nickname(username, nickname_raw)
-        # 从后台直接获取的昵称中，若该昵称含有emoji，该emoji将显示为一张图片，且存放在额外的标签中。
-        # 为了解决这个问题，需要访问该用户的用户页，从网页标题获取正常的emoji字符。
         post_time_raw = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/time/text()'.format(j))[0]
-        post_time = get_post_time(post_time_raw, thread_id)
         operation = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[2]/span/text()'.format(j))[0]
         operator = tree.xpath(
@@ -121,6 +116,12 @@ for i in range(1, MAX_PAGE + 1):
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[1]'.format(j))[0]
         operation_time = tree.xpath(
             '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[2]'.format(j))[0]
+
+        thread_id = re.findall('.+?(?=\?)', url_params)[0]
+        nickname = get_nickname(username, nickname_raw)
+        # 从后台直接获取的昵称中，若该昵称含有emoji，该emoji将显示为一张图片，且存放在额外的标签中。
+        # 为了解决这个问题，需要访问该用户的用户页，从网页标题获取正常的emoji字符。
+        post_time = get_post_time(post_time_raw, thread_id)
 
         log_entry = {thread_id, title, content_preview, username, nickname,
                      post_time, operation, operator, operation_date, operation_time}
