5fb948927d8d245fa73f49539588bfe0b43fd38a
CatMe0w <CatMe0w@live.cn>
2022-01-28 14:25:57 +0800

    Add bootstrapper

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
+import sys
 import time
+import sqlite3
 import requests
 from lxml import etree
 from pathlib import Path
-import sqlite3
-
-TIEBA_NAME = ''
-BDUSS = ''
-MAX_PAGE_POSTS = 1
-MAX_PAGE_USERS = 1
-MAX_PAGE_BAWU = 1  # 百度的HTML中，原文如此。指“吧务”（人事变动）日志。
 
 
 def get_post_id(url_params, thread_id, title):
@@ -91,221 +86,229 @@ def get_post_time(post_time_raw, thread_id, post_id):
     return '{}-{}-{} {}:{}'.format(year, month, day, hour, minute)
 
 
-print("""
-Starting backstage_uncover
+def main(tieba_name, max_page_posts, max_page_users, max_page_bawu, bduss):
+    print("""
+    Starting backstage_uncover
+    
+    Target: {}
+    Pages of posts: {}
+    Pages of users: {}
+    Pages of bawu: {}
+    
+    Weigh anchor!
+    """.format(tieba_name, max_page_posts, max_page_users, max_page_bawu))
 
-Target: {}
-Pages of posts: {}
-Pages of users: {}
-Pages of bawu: {}
+    Path("./uncover-raw/posts").mkdir(parents=True, exist_ok=True)
+    Path("./uncover-raw/users").mkdir(parents=True, exist_ok=True)
+    Path("./uncover-raw/bawu").mkdir(parents=True, exist_ok=True)
 
-Weigh anchor!
-""".format(TIEBA_NAME, MAX_PAGE_POSTS, MAX_PAGE_USERS, MAX_PAGE_BAWU))
-
-Path("./uncover-raw/posts").mkdir(parents=True, exist_ok=True)
-Path("./uncover-raw/users").mkdir(parents=True, exist_ok=True)
-Path("./uncover-raw/bawu").mkdir(parents=True, exist_ok=True)
-
-conn = sqlite3.connect('uncover.db')
-db = conn.cursor()
-db.execute('''
-    create table post(
-    thread_id numeric not null,
-    post_id numeric,
-    title text not null,
-    content_preview text not null,
-    media text,
-    username text not null,
-    post_time text not null,
-    operation text not null,
-    operator text not null,
-    operation_time text not null);''')
-db.execute('''
-    create table user(
-    avatar text not null,
-    username text not null,
-    operation text not null,
-    duration text,
-    operator text not null,
-    operation_time text not null);''')
-db.execute('''
-    create table bawu(
-    avatar text not null,
-    username text not null,
-    operation text not null,
-    operator text not null,
-    operation_time text not null);''')
-conn.commit()
-
-cookies = {
-    'BDUSS': BDUSS,
-}
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-}
-
-for i in range(1, MAX_PAGE_POSTS + 1):
-    params = (
-        ('stype', ''),
-        ('svalue', ''),
-        ('begin', ''),
-        ('end', ''),
-        ('op_type', ''),
-        ('word', TIEBA_NAME),
-        ('pn', str(i)),
-    )
-
-    while True:
-        try:
-            print("Current page: posts, " + str(i))
-            response = requests.get('http://tieba.baidu.com/bawu2/platform/listPostLog',
-                                    headers=headers, params=params, cookies=cookies, verify=False)
-        except requests.exceptions.Timeout:
-            print("Remote is not responding, sleep for 30s.")
-            time.sleep(30)
-            continue
-        else:
-            break
-    with open('./uncover-raw/posts/{}.html'.format(i), 'wb') as f:
-        f.write(response.content)
-
-    content = response.content.decode('gbk')
-    tree = etree.HTML(content)
-    for j in range(1, 31):
-        try:
-            url_params = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@href'.format(j))[0]
-            title = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@title'.format(j))[0]
-            content_preview = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/div[1]/text()'.format(j))[0][12:]
-            media_list = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/div[2]/ul[1]/li/a/@href'.format(j))
-            username = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[1]/a/text()'.format(j))[0][5:]
-            nickname_raw = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[2]/a/text()'.format(j))[0][4:]
-            post_time_raw = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/time/text()'.format(j))[0]
-            operation = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[2]/span/text()'.format(j))[0]
-            operator = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[3]/a/text()'.format(j))[0]
-            operation_date_raw = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[1]'.format(j))[0]
-            operation_time_raw = tree.xpath(
-                '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[2]'.format(j))[0]
-        except IndexError:
-            continue
-        # 正常情况下，一页日志共有30项记录，但个别页可能出现少于30项的情况
-
-        thread_id = url_params.split('/')[-1].split('?')[0]
-        post_id = get_post_id(url_params, thread_id, title)
-        media = get_media(media_list)
-        post_time = get_post_time(post_time_raw, thread_id, post_id)
-        operation_time = operation_date_raw + ' ' + operation_time_raw
-
-        db.execute('insert into post values(?,?,?,?,?,?,?,?,?,?)',
-                   (thread_id, post_id, title, content_preview, media, username, post_time, operation, operator, operation_time))
+    conn = sqlite3.connect('uncover.db')
+    db = conn.cursor()
+    db.execute('''
+        create table post(
+        thread_id numeric not null,
+        post_id numeric,
+        title text not null,
+        content_preview text not null,
+        media text,
+        username text not null,
+        post_time text not null,
+        operation text not null,
+        operator text not null,
+        operation_time text not null);''')
+    db.execute('''
+        create table user(
+        avatar text not null,
+        username text not null,
+        operation text not null,
+        duration text,
+        operator text not null,
+        operation_time text not null);''')
+    db.execute('''
+        create table bawu(
+        avatar text not null,
+        username text not null,
+        operation text not null,
+        operator text not null,
+        operation_time text not null);''')
     conn.commit()
 
-for i in range(1, MAX_PAGE_USERS + 1):
-    params = (
-        ('stype', ''),
-        ('svalue', ''),
-        ('begin', ''),
-        ('end', ''),
-        ('op_type', ''),
-        ('word', TIEBA_NAME),
-        ('pn', str(i)),
-    )
+    cookies = {
+        'BDUSS': bduss,
+    }
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+    }
 
-    while True:
-        try:
-            print("Current page: users, " + str(i))
-            response = requests.get('http://tieba.baidu.com/bawu2/platform/listUserLog',
-                                    headers=headers, params=params, cookies=cookies, verify=False)
-        except requests.exceptions.Timeout:
-            print("Remote is not responding, sleep for 30s.")
-            time.sleep(30)
-            continue
-        else:
-            break
-    with open('./uncover-raw/users/{}.html'.format(i), 'wb') as f:
-        f.write(response.content)
+    for i in range(1, max_page_posts + 1):
+        params = (
+            ('stype', ''),
+            ('svalue', ''),
+            ('begin', ''),
+            ('end', ''),
+            ('op_type', ''),
+            ('word', tieba_name),
+            ('pn', str(i)),
+        )
 
-    content = response.content.decode('gbk')
-    tree = etree.HTML(content)
-    for j in range(1, 31):
-        try:
-            avatar = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/img/@src'.format(j))[0]
-            username = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/text()'.format(j))[0][36:]
-            operation = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[3]/text()'.format(j))[0]
-            duration = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[4]/text()'.format(j))[0][60:-28]  # 这么阴间的HTML到底是怎么产生的？
-            operator = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[5]/a/text()'.format(j))[0]
-            operation_time = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[6]/text()'.format(j))[0]
-        except IndexError:
-            continue
-        # 同上
+        while True:
+            try:
+                print("Current page: posts, " + str(i))
+                response = requests.get('http://tieba.baidu.com/bawu2/platform/listPostLog',
+                                        headers=headers, params=params, cookies=cookies, verify=False)
+            except requests.exceptions.Timeout:
+                print("Remote is not responding, sleep for 30s.")
+                time.sleep(30)
+                continue
+            else:
+                break
+        with open('./uncover-raw/posts/{}.html'.format(i), 'wb') as f:
+            f.write(response.content)
 
-        if duration == '--' or duration == '':
-            duration = None
+        content = response.content.decode('gbk')
+        tree = etree.HTML(content)
+        for j in range(1, 31):
+            try:
+                url_params = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@href'.format(j))[0]
+                title = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/h1/a/@title'.format(j))[0]
+                content_preview = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/div[1]/text()'.format(j))[0][12:]
+                media_list = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[2]/div[2]/ul[1]/li/a/@href'.format(j))
+                username = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[1]/a/text()'.format(j))[0][5:]
+                nickname_raw = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/div[2]/a/text()'.format(j))[0][4:]
+                post_time_raw = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[1]/article/div[1]/time/text()'.format(j))[0]
+                operation = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[2]/span/text()'.format(j))[0]
+                operator = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[3]/a/text()'.format(j))[0]
+                operation_date_raw = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[1]'.format(j))[0]
+                operation_time_raw = tree.xpath(
+                    '//*[@id="container"]/div[2]/div[2]/table/tbody/tr[{}]/td[4]/text()[2]'.format(j))[0]
+            except IndexError:
+                continue
+            # 正常情况下，一页日志共有30项记录，但个别页可能出现少于30项的情况
 
-        db.execute('insert into user values(?,?,?,?,?,?)',
-                   (avatar, username, operation, duration, operator, operation_time))
-    conn.commit()
+            thread_id = url_params.split('/')[-1].split('?')[0]
+            post_id = get_post_id(url_params, thread_id, title)
+            media = get_media(media_list)
+            post_time = get_post_time(post_time_raw, thread_id, post_id)
+            operation_time = operation_date_raw + ' ' + operation_time_raw
 
-for i in range(1, MAX_PAGE_BAWU + 1):
-    params = (
-        ('stype', ''),
-        ('svalue', ''),
-        ('begin', ''),
-        ('end', ''),
-        ('op_type', ''),
-        ('word', TIEBA_NAME),
-        ('pn', str(i)),
-    )
+            db.execute('insert into post values(?,?,?,?,?,?,?,?,?,?)',
+                       (thread_id, post_id, title, content_preview, media, username, post_time, operation, operator, operation_time))
+        conn.commit()
 
-    while True:
-        try:
-            print("Current page: bawu, " + str(i))
-            response = requests.get('http://tieba.baidu.com/bawu2/platform/listBawuLog',
-                                    headers=headers, params=params, cookies=cookies, verify=False)
-        except requests.exceptions.Timeout:
-            print("Remote is not responding, sleep for 30s.")
-            time.sleep(30)
-            continue
-        else:
-            break
-    with open('./uncover-raw/bawu/{}.html'.format(i), 'wb') as f:
-        f.write(response.content)
+    for i in range(1, max_page_users + 1):
+        params = (
+            ('stype', ''),
+            ('svalue', ''),
+            ('begin', ''),
+            ('end', ''),
+            ('op_type', ''),
+            ('word', tieba_name),
+            ('pn', str(i)),
+        )
 
-    content = response.content.decode('gbk')
-    tree = etree.HTML(content)
-    for j in range(1, 31):
-        try:
-            avatar = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/img/@src'.format(j))[0]
-            username = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/text()'.format(j))[0][32:]
-            operation = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[2]/text()'.format(j))[0]
-            operator = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[3]/a/text()'.format(j))[0]
-            operation_time = tree.xpath(
-                '//*[@id="dataTable"]/tbody/tr[{}]/td[4]/text()'.format(j))[0]
-        except IndexError:
-            continue
-        # 同上
+        while True:
+            try:
+                print("Current page: users, " + str(i))
+                response = requests.get('http://tieba.baidu.com/bawu2/platform/listUserLog',
+                                        headers=headers, params=params, cookies=cookies, verify=False)
+            except requests.exceptions.Timeout:
+                print("Remote is not responding, sleep for 30s.")
+                time.sleep(30)
+                continue
+            else:
+                break
+        with open('./uncover-raw/users/{}.html'.format(i), 'wb') as f:
+            f.write(response.content)
 
-        db.execute('insert into bawu values(?,?,?,?,?)',
-                   (avatar, username, operation, operator, operation_time))
-    conn.commit()
+        content = response.content.decode('gbk')
+        tree = etree.HTML(content)
+        for j in range(1, 31):
+            try:
+                avatar = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/img/@src'.format(j))[0]
+                username = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/text()'.format(j))[0][36:]
+                operation = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[3]/text()'.format(j))[0]
+                duration = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[4]/text()'.format(j))[0][60:-28]  # 这么阴间的HTML到底是怎么产生的？
+                operator = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[5]/a/text()'.format(j))[0]
+                operation_time = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[6]/text()'.format(j))[0]
+            except IndexError:
+                continue
+            # 同上
 
-conn.close()
+            if duration == '--' or duration == '':
+                duration = None
+
+            db.execute('insert into user values(?,?,?,?,?,?)',
+                       (avatar, username, operation, duration, operator, operation_time))
+        conn.commit()
+
+    for i in range(1, max_page_bawu + 1):
+        params = (
+            ('stype', ''),
+            ('svalue', ''),
+            ('begin', ''),
+            ('end', ''),
+            ('op_type', ''),
+            ('word', tieba_name),
+            ('pn', str(i)),
+        )
+
+        while True:
+            try:
+                print("Current page: bawu, " + str(i))
+                response = requests.get('http://tieba.baidu.com/bawu2/platform/listBawuLog',
+                                        headers=headers, params=params, cookies=cookies, verify=False)
+            except requests.exceptions.Timeout:
+                print("Remote is not responding, sleep for 30s.")
+                time.sleep(30)
+                continue
+            else:
+                break
+        with open('./uncover-raw/bawu/{}.html'.format(i), 'wb') as f:
+            f.write(response.content)
+
+        content = response.content.decode('gbk')
+        tree = etree.HTML(content)
+        for j in range(1, 31):
+            try:
+                avatar = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/img/@src'.format(j))[0]
+                username = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[1]/a/text()'.format(j))[0][32:]
+                operation = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[2]/text()'.format(j))[0]
+                operator = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[3]/a/text()'.format(j))[0]
+                operation_time = tree.xpath(
+                    '//*[@id="dataTable"]/tbody/tr[{}]/td[4]/text()'.format(j))[0]
+            except IndexError:
+                continue
+            # 同上
+
+            db.execute('insert into bawu values(?,?,?,?,?)',
+                       (avatar, username, operation, operator, operation_time))
+        conn.commit()
+
+    conn.close()
+
+
+if __name__ == '__main__':
+    if not sys.argv[5:]:
+        print('Usage: python3 {} <tieba_name> <max_page_posts> <max_page_users> <max_page_bawu> <bduss>'.format(sys.argv[0]))
+        exit(1)
+    main(str(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), str(sys.argv[5]))
